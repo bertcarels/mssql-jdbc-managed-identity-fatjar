b278fc1056b4d574eb96ac3389cad8e675044e30
Bert Carels <bertcarels@gmail.com>
2023-11-16 17:01:38 +0100

    initial commit

---
+++ src/main/java/org/example/AzureSQLManagedIdentityExample.java
@@ -0,0 +1,57 @@
+package org.example;
+
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class AzureSQLManagedIdentityExample {
+    public static void main(String[] args) {
+        try {
+
+            Logger logger = LoggerFactory.getLogger(AzureSQLManagedIdentityExample.class);
+
+            logger.info("Running query on:");
+            logger.info("server: " + args[0]);
+            logger.info("db: " + args[1]);
+            logger.info("table: " + args[2]);
+
+
+
+
+
+
+            // Set up the Azure SQL Database DataSource
+            SQLServerDataSource ds = new SQLServerDataSource();
+            ds.setServerName(args[0] +".database.windows.net");
+            ds.setDatabaseName(args[1]);
+            ds.setAuthentication("ActiveDirectoryMSI");
+
+            // Establish the connection
+            Connection connection = ds.getConnection();
+
+            // Execute a simple query
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT TOP 5 * FROM " + args[2] );
+
+            // Process the results
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
+
+            // Close resources
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
